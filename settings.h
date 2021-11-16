50da7099e4c98294012d8b47c984d1d2443238df
pnicewicz <pnicewicz@solwit.com>
2021-11-16 22:01:26 +0100

    Update settings.h

--- settings.h
+++ settings.h
@@ -42,8 +42,8 @@ result set and select the entry closest to the actual location you want to displ
 data for. It'll be a URL like https://openweathermap.org/city/2657896. The number
 at the end is what you assign to the constant below.
  */
-String OPEN_WEATHER_MAP_LOCATION_ID = "2657896";
-String DISPLAYED_LOCATION_NAME = "Zurich";
+String OPEN_WEATHER_MAP_LOCATION_ID = "3099434";
+String DISPLAYED_LOCATION_NAME = "Gdansk";
 /*
 Arabic -> ar, Bulgarian -> bg, Catalan -> ca, Czech -> cz, German -> de, Greek -> el,
 English -> en, Persian (Farsi) -> fa, Finnish -> fi, French -> fr, Galician -> gl,
@@ -53,14 +53,14 @@ Portuguese -> pt, Romanian -> ro, Russian -> ru, Swedish -> se, Slovak -> sk,
 Slovenian -> sl, Spanish -> es, Turkish -> tr, Ukrainian -> ua, Vietnamese -> vi,
 Chinese Simplified -> zh_cn, Chinese Traditional -> zh_tw.
 */
-const String OPEN_WEATHER_MAP_LANGUAGE = "en";
+const String OPEN_WEATHER_MAP_LANGUAGE = "pl";
 
 // Adjust according to your language
-const String WDAY_NAMES[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-const String MONTH_NAMES[] = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
-const String SUN_MOON_TEXT[] = {"Sun", "Rise", "Set", "Moon", "Age", "Illum"};
-const String MOON_PHASES[] = {"New Moon", "Waxing Crescent", "First Quarter", "Waxing Gibbous",
-                              "Full Moon", "Waning Gibbous", "Third quarter", "Waning Crescent"};
+const String WDAY_NAMES[] = {"niedz.", "pon.", "wto.", "sr.", "czw.", "pt.", "sob."};
+const String MONTH_NAMES[] = {"STY", "LUT", "MAR", "KWI", "MAJ", "CZE", "LIP", "SIE", "WRZ", "PAŹ", "LIS", "GRU"};
+const String SUN_MOON_TEXT[] = {"Słońce", "Wschód", "Zachód", "Księżyc", "Wiek", "Świecenie"};
+const String MOON_PHASES[] = {"Nów", "Pierwsza Kwarta", "Druga kwarta", "Półksiężyc",
+                              "Pełnia", "Ubywający księżyc", "Ostatnia kwarta"};
 
 // pick one from TZinfo.h
 String TIMEZONE = getTzInfo("Europe/Zurich");
